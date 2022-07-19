f199350f8bf1b43f9b91a12f92c6fb8a0a896469
Gaurav <Gaurav@GAURAV>
2022-07-19 09:21:14 +0530

    Add project files.

---
+++ CRUDUsingADO/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUDUsingADO.Models
+{
+    [Table("Product")]
+    public class Product
+    { 
+        [Key]
+        [ScaffoldColumn(false)]
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Product name is required")]
+        [DataType(DataType.Text)]
+        [Display(Name="Product Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Price is required")]
+        [Display(Name="Product Price")]
+        [Range(minimum:1,maximum:50000)]
+        public double Price { get; set; }
+    }
+}
